ec0574c7e8b06c966cf7d6aff85261f50b763428
Teja Theegala <sriteja23@gmail.com>
2015-05-30 14:23:10 +0530

    Changed ControllerModule.py

--- ControllerModule.py
+++ ControllerModule.py
@@ -10,31 +10,3 @@ class ControllerModule:
     def processCBT(self):
         pass
 
-'''# Sample Controller Module 1
-class ModuleA1(ControllerModule):
-
-	def __init__(self,cfxObject,paramDict):
-		self.cfxObject = cfxObject
-		self.paramDict = paramDict
-
-	 # Main processing loop
-	def processCBT(self):
-		while(True): # Polling approach
-			cbt = self.cfxObject.getCBT() # 
-			# Process the CBT here
-			# Analyse CBT. If heavy, run it on another thread
-
-
-
-# Sample Controller Module 2
-class ModuleC3(ControllerModule):
-
-	def __init__(self,cfxObject,paramDict):
-		self.cfxObject = cfxObject
-		self.paramDict = paramDict
-
-	def processCBT(self): # Main processing loop
-		while(True): # Polling approach
-			cbt = self.cfxObject.getCBT() # 
-			# Process the CBT here
-			# Analyse CBT. If heavy, run it on another thread'''
