7a898c905b8595b75dc138dcbfad23a9e4c21f88
Teja Theegala <sriteja23@gmail.com>
2015-07-22 00:32:17 +0530

    Modified config.json

--- BaseTopologyManager.py
+++ BaseTopologyManager.py
@@ -21,7 +21,7 @@ class BaseTopologyManager(ControllerModule):
         self.CFxHandle.submitCBT(logCBT)
 
    def processCBT(self,cbt):
-   	
+    
         # In case of a fresh CBT, request the required services
         # from the other modules, by issuing CBTs. If no services
         # from other modules required, process the CBT here only
